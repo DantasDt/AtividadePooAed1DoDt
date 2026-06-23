9dabe44411e94bf748884b274eb720d1763efb8c
Dantas <dantas.69.g@gmail.com>
2026-06-23 19:08:58 -0300

    Update AtvListasDoDt.py

--- AtvListasDoDt.py
+++ AtvListasDoDt.py
@@ -49,9 +49,54 @@ for i in range(100):
     soma += num
 media = soma / 100
 ord = sorted(numeros)
-print(f"Lista original\n{ord}")
+print(f"Lista organizada\n{ord}")
 print(f"Lista Normal\n{numeros}")
 print(f"Indice maior {numeros.index(max(numeros))}")
 print(f"Indice menor {numeros.index(min(numeros))}")
 print(f"Soma {soma}")
 print(f"Média {media}")
+
+# Atividade 4
+
+l1 = []
+l2 = []
+l3 = []
+
+p1 = int(input("Informe a quantidade de elementos da l1: "))
+for i in range(p1):
+    x = int(input("Valor: "))
+    l1.append(x)
+
+p2 = int(input("Informe a quantidade de elementos da l2: "))
+for i in range(p2):
+    y = int(input("Valor: "))
+    l2.append(y)
+
+i = 0
+while i < len(l1) or i < len(l2):
+    if i < len(l1):
+        l3.append(l1[i])
+
+    if i < len(l2):
+        l3.append(l2[i])
+    i += 1
+print(f"Lista 1: {l1}")
+print(f"Lista 2: {l2}")
+print(f"Lista 3: {l3}")
+
+# Atividade 5
+
+l4 = []
+l5 = []
+t1 = []
+
+for i in range(20):
+    l4.append(randint(0, 50))
+    l5.append(randint(0, 50))
+
+for i in l4:
+    if i in l5 and i not in t1:
+        t1.append(i)
+print(f"\nLista 4: {l4}")
+print(f"Lista 5: {l5}")
+print(f"Interseção: {t1}")
