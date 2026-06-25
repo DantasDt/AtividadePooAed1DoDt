48827e4e41c7b75fc5992f79e11d5fe0b71a9435
Dantas <dantas.69.g@gmail.com>
2026-06-25 15:07:13 -0300

    Update Dicionario.py

--- Dicionario.py
+++ Dicionario.py
@@ -25,3 +25,34 @@ for palavra in script:
         else:
             dicionario[palavra] = 1
 print(f"\nDicionário\n{dicionario}")
+
+# Atividade 3
+d1 = {"a" : 1, "b" : 5, "c": 3}
+d2 = {"a" : 2, "q" : 4, "d" : 12}
+d3 = {}
+def mesclar(d1, d2):
+    for componente in d1:
+        for comp2 in d2:
+            if componente == comp2:
+                if d1[componente] > d2[comp2]:
+                    d3[componente] = d1[componente]
+                else:
+                    d3[componente] = d2[comp2]
+            else:
+                d3[componente] = d1[componente]
+                d3[comp2] = d2[comp2]
+
+mesclar(d1, d2)
+print(f"\nDicionário final\n{d3}")
+
+# Atividade 4
+dados = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+chaves_filtradas = ['a', 'c', 'e']
+completo = {}
+def filtrar_dicionario(dados, chaves_filtradas):
+    for componente in dados:
+        if componente in chaves_filtradas:
+            completo[componente] = dados[componente]
+
+filtrar_dicionario(dados, chaves_filtradas)
+print(f"\nCompleto\n{completo}")
